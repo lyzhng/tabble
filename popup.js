f36b86295a4dd74abc629a41d42c43a57162867c
Lily Zhong <lilyzhongy@gmail.com>
2020-07-04 14:15:30 -0400

    Remove listener from popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,5 @@
 console.log('popup.js is running!');
 
-browser.runtime.onMessage.addListener((msg, sender) => {
-  console.log('Data listened in popup.js', msg);
-  return Promise.resolve('donezo');
-});
-
 browser.tabs.getCurrent(async (tab) => {
   const tabList = await browser.runtime.sendMessage('get_tabs');
   console.log(tabList);
