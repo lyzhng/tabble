447e10a56011724387544939fa52c1d753da9602
Lily Zhong <lilyzhongy@gmail.com>
2020-07-05 13:20:24 -0400

    Remove popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-console.log('popup.js is running!');
-
-browser.tabs.getCurrent(async (tab) => {
-  const tabList = await browser.runtime.sendMessage('get_tabs');
-  console.log(tabList);
-});
-
-// when user goes to extension URL (not just when popup)
