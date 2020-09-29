4a2a8a357ac4e30c3c07efa3b5b47e32359dec67
Lily Zhong <lilyzhongy@gmail.com>
2020-09-29 01:41:46 -0400

    Set color-scheme in local storage and add styling

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,26 +1,85 @@
 <template>
-  <h1>
-    {{ heading }}
-    <img
-      @click.prevent.stop="toggleColorScheme"
-      :src="
-        lightColorScheme
-          ? 'https://api.iconify.design/uil-sun.svg?color=black'
-          : 'https://api.iconify.design/ion-moon.svg?color=white'
-      "
-      width="32"
-      height="32"
-    />
-  </h1>
+  <nav>
+    <h1 id="heading">
+      {{ heading }}
+      <img
+        @click.prevent.stop="toggleColorScheme"
+        :src="
+          colorScheme === 'light'
+            ? 'https://api.iconify.design/uil-sun.svg?color=black'
+            : 'https://api.iconify.design/ion-moon.svg?color=yellow'
+        "
+        width="32"
+        height="32"
+      />
+    </h1>
+    <SearchBar />
+  </nav>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Fragment } from 'vue-fragment';
+import SearchBar from './SearchBar';
+import { mapGetters } from 'vuex';
 
-@Component
+@Component({
+  components: {
+    SearchBar,
+  },
+})
 export default class NavigationBar extends Vue {
-  lightColorScheme: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+  colorScheme: string = null;
   readonly heading: string = 'tabble, your tab manager';
+
+  mounted() {
+    this.initColorScheme();
+    this.initColorSchemeHandler();
+  }
+
+  initColorScheme() {
+    this.colorScheme = localStorage.getItem('color-scheme') ?? this.colorScheme;
+    document.body.setAttribute('color-scheme', this.colorScheme);
+  }
+
+  toggleColorScheme() {
+    switch (this.colorScheme) {
+      case 'light':
+        this.colorScheme = 'dark';
+        break;
+      case 'dark':
+        this.colorScheme = 'light';
+        break;
+    }
+    localStorage.setItem('color-scheme', this.colorScheme);
+    document.body.setAttribute('color-scheme', this.colorScheme);
+  }
+
+  initColorSchemeHandler() {
+    const mql = window.matchMedia('(prefers-color-scheme: light)');
+    mql.addEventListener('change', (e) => {
+      if (e.matches) {
+        this.colorScheme = 'light';
+      } else {
+        this.colorScheme = 'dark';
+      }
+      localStorage.setItem('color-scheme', this.colorScheme);
+      document.body.setAttribute('color-scheme', this.colorScheme);
+    });
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+nav {
+  text-align: center;
+}
+
+img {
+  vertical-align: middle;
+  &:hover,
+  &:focus {
+    cursor: pointer;
+  }
+}
+</style>
