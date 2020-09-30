5b8311fb2a6afd15da2e3dadbd19cedbcf2b4f1d
Lily Zhong <lilyzhongy@gmail.com>
2020-09-30 10:25:14 -0400

    Clean up SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,24 +3,25 @@
 </template>
 
 <script charset="utf-8" lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
-import { Fragment } from 'vue-fragment';
-import { Message } from '../utils/constants';
+  import { Component, Vue, Watch } from 'vue-property-decorator';
+  import { browser } from 'webextension-polyfill-ts';
 
-@Component
-export default class SearchBar extends Vue {
-  query: string = '';
+  import { Message } from '../utils/constants';
 
-  async mounted() {
-    await this.searchForTabs();
+  @Component
+  export default class SearchBar extends Vue {
+    query: string = '';
+
+    async mounted() {
+      await this.searchForTabs();
+    }
+
+    async searchForTabs() {
+      const res = await browser.runtime.sendMessage({
+        msg: Message.GET_TABS,
+        query: this.query,
+      });
+      this.$root.$emit('setTabs', res.data.tabs);
+    }
   }
-
-  async searchForTabs() {
-    const res = await browser.runtime.sendMessage({
-      msg: Message.GET_TABS,
-      query: this.query,
-    });
-    this.$root.$emit('setTabs', res.data.tabs);
-  }
-}
 </script>
