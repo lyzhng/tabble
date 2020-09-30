2f0493b3592e1641434f14b4b8efaefb771180e0
Lily Zhong <lilyzhongy@gmail.com>
2020-09-30 14:02:51 -0400

    (WIP) Add tab indices, aria-labels, alt, roles, and skiplinks

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,88 @@
 <template>
-  <main id="app">
-    <NavigationBar />
-    <TabList />
-  </main>
+  <div id="container">
+    <ul id="skiplinks">
+      <li>
+        <a href="#container">Go back to top</a>
+      </li>
+      <li>
+        <a href="#search">Skip to search</a>
+      </li>
+      <li>
+        <a href="#main">Skip to main</a>
+      </li>
+    </ul>
+    <div id="content">
+      <NavigationBar />
+      <TabList />
+    </div>
+  </div>
 </template>
 
 <script charset="utf-8">
-import TabList from './components/TabList.vue';
-import NavigationBar from './components/NavigationBar';
+  import TabList from './components/TabList.vue';
+  import NavigationBar from './components/NavigationBar';
 
-export default {
-  name: 'App',
-  methods: {
-    initKeyHandler() {
-      document.addEventListener('keydown', ({ key }) => {
-        const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        const scrollAmt = height * 0.15;
-        switch (key) {
-          case 'j':
-            window.scrollBy({
-              top: scrollAmt,
-              behavior: 'smooth',
-            });
-            break;
-          case 'k':
-            window.scrollBy({
-              top: -scrollAmt,
-              behavior: 'smooth',
-            });
-            break;
-          case 'G':
-            window.scrollBy({
-              top: document.body.scrollHeight,
-              behavior: 'smooth',
-            });
-            break;
-        }
-      });
+  export default {
+    name: 'App',
+    components: {
+      TabList,
+      NavigationBar,
     },
-  },
-  mounted() {
-    this.initKeyHandler();
-  },
-  components: {
-    TabList,
-    NavigationBar,
-  },
-};
+    mounted() {
+      this.initKeyHandler();
+    },
+    methods: {
+      initKeyHandler() {
+        document.addEventListener('keydown', ({ key }) => {
+          const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+          const scrollAmt = height * 0.15;
+          switch (key) {
+            case 'j':
+              window.scrollBy({
+                top: scrollAmt,
+                behavior: 'smooth',
+              });
+              break;
+            case 'k':
+              window.scrollBy({
+                top: -scrollAmt,
+                behavior: 'smooth',
+              });
+              break;
+            case 'G':
+              window.scrollBy({
+                top: document.body.scrollHeight,
+                behavior: 'smooth',
+              });
+              break;
+          }
+        });
+      },
+    },
+  };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap');
-$font-primary: 'Roboto Mono', monospace;
-main {
-  width: 65vw;
-  min-height: 100vh;
-  margin: 0 auto;
-  padding: 0.5rem 1rem;
-  font-family: $font-primary;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap');
+  $font-primary: 'Roboto Mono', monospace;
+  #container {
+    font-family: $font-primary;
+  }
+  #content {
+    width: 65vw;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: 0.5rem 1rem;
+  }
+  #skiplinks {
+    position: fixed;
+    list-style-type: none;
+    padding: 0;
+    margin: 0.5rem;
+    line-height: 1.25;
+
+    & a:focus {
+      display: inline-block;
+    }
+  }
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,27 +1,29 @@
 <template>
-  <input type="text" v-model="query" @input="searchForTabs" placeholder="Search" id="searchbox" />
+  <form @submit.prevent.stop role="search">
+    <input type="text" v-model="query" @input="searchForTabs" placeholder="Search" id="search" />
+  </form>
 </template>
 
 <script charset="utf-8" lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { browser } from 'webextension-polyfill-ts';
+  import { Component, Vue } from 'vue-property-decorator';
+  import { browser } from 'webextension-polyfill-ts';
 
-import { Message } from '../utils/constants';
+  import { Message } from '../utils/constants';
 
-@Component
-export default class SearchBar extends Vue {
-  query: string = '';
+  @Component
+  export default class SearchBar extends Vue {
+    query: string = '';
 
-  async mounted() {
-    await this.searchForTabs();
+    async mounted() {
+      await this.searchForTabs();
+    }
+
+    async searchForTabs() {
+      const res = await browser.runtime.sendMessage({
+        msg: Message.GET_TABS,
+        query: this.query,
+      });
+      this.$root.$emit('setTabs', res.data.tabs);
+    }
   }
-
-  async searchForTabs() {
-    const res = await browser.runtime.sendMessage({
-      msg: Message.GET_TABS,
-      query: this.query,
-    });
-    this.$root.$emit('setTabs', res.data.tabs);
-  }
-}
 </script>
